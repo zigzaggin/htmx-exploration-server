0ced8cb31dc739e54bc9e2aa9fccb98d9f823a23
Ben Potter <benjamin.potter@live.com>
2024-02-10 22:11:24 -0700

    Exploring HTMX Server

---
+++ routes/users.js
@@ -0,0 +1,25 @@
+var express = require('express');
+var db = require('../db/db');
+var router = express.Router();
+
+/* GET users listing. */
+router.get('/', function (req, res, next) {
+    let users = db;
+    if (req.query.search) {
+        users = db.filter(v => v.userName.toLowerCase().includes(req.query.search.toLowerCase()));
+    }
+    res.render('users', {users: users})
+});
+
+router.post('/', (req, res) => {
+    let user = {userName: req.body.userName};
+    db.unshift(user)
+    res.render('user', {user})
+});
+
+router.delete('/', (req, res) => {
+    db = db.filter(v => v.userName !== req.query.userName);
+    res.send();
+});
+
+module.exports = router;
